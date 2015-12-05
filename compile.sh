7c6a001749b187eadd6a5a076a408d56ed9a5bb1
cagdasoztekin <cagdas.oztekin@ug.bilkent.edu.tr>
2015-12-05 22:14:06 +0200

    finish code and update script and create analysis

--- compile.sh
+++ compile.sh
@@ -1,4 +1,21 @@
-gcc -Wall -pg  lbp_seq.c util.h util.c -o lbp_seq
-./lbp_seq
-gprof lbp_seq gmon.out > analysis-sequential.txt
+#!/bin/bash
+k_vals=(1 2 5 7 10)
+rm analysis-sequential.txt
+rm analysis-parallel.txt
+touch analysis-parallel.txt
+touch analysis-sequential.txt
+for i in ${k_vals[@]}; 
+	do
+	echo "Running sequential program for k = ${i}"
+	gcc -Wall -pg  lbp_seq.c util.h util.c -o lbp_seq
+	./lbp_seq ${i}
+	echo "Results for k=${i} \n" >> analysis-sequential.txt
+	gprof lbp_seq gmon.out >> analysis-sequential.txt
+	echo "Running parallel program for k = ${i}"
+	gcc -Wall -pg  lbp_omp.c util.h util.c -o lbp_omp -fopenmp
+	./lbp_omp ${i}
+	echo "Results for k=${i} \n" >> analysis-parallel.txt
+	gprof lbp_omp gmon.out >> analysis-parallel.txt
+	done
+
 
--- lbp_omp.c
+++ lbp_omp.c
@@ -1,3 +1,4 @@
+#include <omp.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
@@ -5,26 +6,28 @@
 #include "util.h"
 
 #define num_people 18
-#define num_files 10		// this is 'k' as defined in the assignment
+// #define num_files 10		// this is 'k' as defined in the assignment
 #define total_num_files 20 
 #define rows 180
 #define cols 200
 
+int num_files;
 
+void create_histogram(int* hist, int** img, int num_rows, int num_cols);
+int find_closest(int*** training_set, int num_persons, int num_training, int size, int* test_image);
 
-int main(){
+int main(int argc, char** argv){
 
 	int i, j;
-
-
-
+	int b; 
+	num_files = atoi(argv[1]);
 
 	// 4d array to have 2 layers for height and width and 2 layers for files and people
 	int**** pre_training_set = (int****)malloc(num_people * sizeof(int***));
 	for(i = 0; i < num_people; i++){
 		pre_training_set[i] = (int***)malloc(num_files * sizeof(int**));
 	}
-	char filename[97];
+	char filename[128];
 	// printf("Hello\n");
 	for(i = 0; i < num_people; i++){
 		for(j = 0; j < num_files; j++){
@@ -45,7 +48,10 @@ int main(){
 	for(i = 0; i < num_people; i++){
 		for(j = 0; j < num_files; j++){
 			// printf("Onto it with i %d j %d\n", i, j);
-			training_set[i][j] = (int*)malloc(sizeof(int) * (rows-2)*(cols-2));
+			training_set[i][j] = (int*)malloc(sizeof(int) * 256);
+			for(b = 0; b < 256; b++){
+				training_set[i][j][b] = 0;
+			}
 			create_histogram(training_set[i][j], pre_training_set[i][j], rows, cols);
 
 		}
@@ -58,6 +64,7 @@ int main(){
 	}
 
 	// printf("pre test set\n");
+
 
 	for(i = 0; i < num_people; i++){
 		for(j = 0; j < (total_num_files - num_files); j++){
@@ -72,7 +79,10 @@ int main(){
 	for(i = 0; i < num_people; i++){
 		test_set[i] = (int**)malloc((total_num_files - num_files) * sizeof(int*));
 		for(j = 0; j < total_num_files - num_files; j++){
-			test_set[i][j] = (int*)malloc(sizeof(int) * (rows-2)*(cols-2));
+			test_set[i][j] = (int*)malloc(sizeof(int) * 256);
+			for(b = 0; b < 256; b++){
+				test_set[i][j][b] = 0;
+			}
 			create_histogram(test_set[i][j], pre_test_set[i][j], rows, cols);
 		}
 	}
@@ -81,10 +91,12 @@ int main(){
 	int falseCount = 0;
 	for(i = 0; i < num_people; i++){
 		for(j = 0; j < total_num_files - num_files; j++){
-			int cur = find_closest(training_set, num_people, num_files, (rows-2)*(cols-2), test_set[i][j]);
+			int cur = find_closest(training_set, num_people, num_files, 256, test_set[i][j]);
+
 			// test the file with file name "%d.%d.txt\t\t%d\t\t%d\n", i+1, j+1+num_files
 			totalCount++;	// a judgment is made
 			if(cur != i+1){ // a false judgment is made
+				printf("%d.%d.txt\t\t%d\t\t%d\n", i+1, j+1+num_files, i+1, cur);
 				falseCount += 1;
 			}
 		}
@@ -92,6 +104,51 @@ int main(){
 	printf("%d false out of %d\n", falseCount, totalCount);
 
 	// TODO:: deallocate the allocated memory space
+
+	int a;
+
+	for(i = 0; i < num_people; i++){
+		for(j = 0; j < num_files; j++){
+			for(a = 0; a < rows; a++){
+				free(pre_training_set[i][j][a]);
+			}
+			free(pre_training_set[i][j]);
+		}
+		free(pre_training_set[i]);
+	}
+	free(pre_training_set);
+	// printf("free pre training set\n");
+
+	for(i = 0; i < num_people; i++){
+		for(j = 0; j < total_num_files - num_files; j++){
+			for(a = 0; a < rows; a++){
+				free(pre_test_set[i][j][a]);
+			}
+			free(pre_test_set[i][j]);
+		}
+		free(pre_test_set[i]);
+	}
+	free(pre_test_set);
+	// printf("free pre test set\n");
+
+	for(i = 0; i < num_people; i++){
+		for(j = 0; j < num_files; j++){
+			free(training_set[i][j]);
+		}
+		free(training_set[i]);
+	}
+	free(training_set);
+	// printf("free training set\n");
+
+	for(i = 0; i < num_people; i++){
+		for(j = 0; j < total_num_files - num_files; j++){
+			free(test_set[i][j]);
+		}
+		free(test_set[i]);
+	}
+	free(test_set);
+	// printf("free test set\n");
+
 
 	return 0;
 }
@@ -115,13 +172,23 @@ int toDecimal(int binary){
 void create_histogram(int* hist, int** img, int num_rows, int num_cols){
 	// printf("Hola histo\n");
 	int i, j;
+
+	int base = 1;
+	int k;
+	int currentBinary = 0;
 	// hist = (int*)malloc(sizeof(int) * (num_rows - 2) * (num_cols - 2));
+	
+	// #pragma omp parallel for private(i, j)
+	
 	for(i = 1; i < num_rows - 1; i++){
+		// #pragma omp parallel for private(base, k, currentBinary)
+		// #pragma omp parallel for schedule(dynamic)
+		#pragma omp parallel for private(k) firstprivate(base, currentBinary)
 		for(j = 1; j < num_cols - 1; j++){
 
-			int currentBinary = 0;
-			int base = 1;
-			int k = img[i][j];
+			currentBinary = 0;
+			base = 1;
+			k = img[i][j];
 
 			currentBinary += (k < img[i][j-1]) * base;
 			base *= 2;
@@ -140,7 +207,8 @@ void create_histogram(int* hist, int** img, int num_rows, int num_cols){
 			currentBinary += (k < img[i-1][j-1]) * base;
 			// printf("Current binary is %d\n", toDecimal(currentBinary));
 			// hist[(i-1)*(num_cols-2) + (j-1)] = toDecimal(currentBinary);
-			hist[(i-1)*(num_cols-2) + (j-1)] = currentBinary;
+			#pragma omp atomic
+			hist[currentBinary] += 1;
 		}
 	}
 }
@@ -149,13 +217,14 @@ void create_histogram(int* hist, int** img, int num_rows, int num_cols){
 double distance(int* a, int* b, int size){
 	int i;
 	double distance = 0;
-	#pragma omp parallel for private(i)
+	#pragma omp parallel for reduction(+:distance)
 	for(i = 0; i < size; i++){
 		// don't change the current sum if both are zero // disrupts the whole thing
 		if(a[i] != 0 || b[i] != 0){
 			distance += (double)(a[i] - b[i])*(a[i] - b[i])/(double)(2*(a[i] + b[i]));
 		}
 	}
+
 	// #pragma omp barrier
 	// printf("This distance is %f\n", distance);
 	return distance;
@@ -164,20 +233,19 @@ double distance(int* a, int* b, int size){
 int find_closest(int*** training_set, int num_persons, int num_training, int size, int* test_image){
 	// printf("Hey");
 	int i, j;
-	double closest = 255 * (size) + 1; // actual farthest possible distance between two histograms and plus one obviously
+	double closest = 256 * size * size + 1; // actual farthest possible distance between two histograms and plus one obviously
 	// printf("Double max is %f", closest);
 	double current = 0;
 	int personid = -1;
-	int tid;
-	#pragma omp parallel for private(i,j, tid)
 	
+
 	for(i = 0; i < num_persons; i++){
-		tid = omp_get_thread_num();
 		// printf("Running on thread %d and i is %d\n", tid, i);
+		// #pragma omp parallel for shared(training_set, test_image, size) private(current)
 		for(j = 0; j < num_files; j++){
 			current = distance(training_set[i][j], test_image, size);
 			// printf("Current distance is %f with i %d and j %d\n", current, i, j);
-			if(current < closest){
+			if(current <= closest){
 				// printf("IFIFIFCurrent distance is %f with i %d and j %d\n", current, i, j);
 				closest = current;
 				personid = i+1;
